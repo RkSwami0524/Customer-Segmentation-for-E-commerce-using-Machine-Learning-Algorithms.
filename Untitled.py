d2f485c72d0c563089f749252661117597778d7e
Rkswami <139205369+RkSwami0524@users.noreply.github.com>
2026-04-04 23:15:08 +0530

    Change CSV file input to use uploaded file

--- Untitled.py
+++ Untitled.py
@@ -25,7 +25,7 @@ st.title("🛍️ Ecommerce Customer Analysis App")
 uploaded_file = st.file_uploader("Upload Ecommerce CSV file", type=["csv"])
 
 if uploaded_file:
-    df = pd.read_csv('Ecommerce Customers.csv')
+    df = pd.read_csv(uploaded_file)
 
     st.subheader("📊 Raw Data")
     st.dataframe(df.head())
